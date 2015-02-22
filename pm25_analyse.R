0d2ccd0b29de3b65ab19c418210c933856a37133
claudionanni <claudio.nanni@gmail.com>
2015-02-22 18:44:35 +0100

    almost done

--- pm25_analyse.R
+++ pm25_analyse.R
@@ -26,7 +26,12 @@ library(plyr)
 # Use ddply to group by "year" and sum "Emissions"
 q1_data <- ddply(NEI_pm25, c("year"), summarise,
                  TotalPerYear    = sum(as.numeric(Emissions)))
-plot(q1_data,col="red",xlab="year",ylab="PM25",cex=1,pch=15,main="United States PM2.5 Emission in years 1999,2002,2005,2008.")
+plot(q1_data,col="red",
+	xlab="Year",
+	ylab="PM25",
+	cex=1.5,
+	pch=15,
+	main="United States PM2.5 Emission in years 1999,2002,2005,2008.")
 
 
 ## > QUESTION 2 "Total emissions from PM2.5 in the Baltimore City, Maryland (fips == "24510") from 1999 to 2008" < #
@@ -37,7 +42,13 @@ NEI_pm25_baltimore <- NEI_pm25[NEI_pm25$fips == "24510",]
 # Use ddply to group by "year" and sum "Emissions"
 q2_data <- ddply(NEI_pm25_baltimore, c("year"), summarise,
                  TotalPerYear    = sum(Emissions))
-plot(q2_data,col="blue",xlab="year",ylab="PM25",cex=1,pch=15,main="Baltimore PM2.5 Emission in years 1999,2002,2005,2008.")
+plot(q2_data,
+	col="blue",
+	xlab="year",
+	ylab="PM25",
+	cex=1.5,
+	pch=15,
+	main="Baltimore PM2.5 Emission in years 1999,2002,2005,2008.")
 
 
 # > QUESTION 3 "Total emissions from PM2.5 in Baltimore from 1999 to 2008 per source type (point, nonpoint, onroad, nonroad)" < #
@@ -52,4 +63,106 @@ qplot(year, TotalPerYear, data = q3_data, geom = "line",
     xlab = "Year",
     ylab = "Total Emissions per Year",
     main = "Baltimore PM2.5 Emission in years 1999,2002,2005,2008 per each source type")
+
+
+# > QUESTION 4 "Total emissions from PM2.5 in United States from 1999 to 2008 from Coal related sources" < #
+
+# Getting the source IDs from the SDD data frame, only for coal combustion-related sources
+coalRelatedSources <- SCC[grep("Coal", SCC$EI.Sector), 'SCC']
+
+###########################################################
+# > unique(SCC[SCC$SCC %in% coalRelatedSources, 4])
+# [1] Fuel Comb - Electric Generation - Coal     
+# [2] Fuel Comb - Industrial Boilers, ICEs - Coal
+# [3] Fuel Comb - Comm/Institutional - Coal      
+# 59 Levels: Agriculture - Crops & Livestock Dust ...
+###########################################################
+# Subset for only Coal related sources
+NEI_pm25_coal <- NEI_pm25[NEI_pm25$SCC %in% coalRelatedSources,]
+
+q4_data <- ddply(NEI_pm25_coal, c("year"), summarise,
+                 TotalPerYear    = sum(as.numeric(Emissions)))
+
+plot(q4_data,col="black",
+	xlab="Year",
+	ylab="PM25",
+	cex=2,
+	pch=18,
+	main="United States PM2.5 Emission in years 1999,2002,2005,2008 from coal combustion-related sources")
+
+
+
+# > QUESTION 5 "Total emissions from PM2.5 in Baltimore from 1999 to 2008 from motor Vehicles" < #
+# Get only Baltimore observations
+NEI_pm25_baltimore <- NEI_pm25[NEI_pm25$fips == "24510",]
+
+# Getting the source IDs from the SDD data frame, only for vehicle combustion-related sources
+motorVehicle <- SCC[grep("Vehicle", SCC$EI.Sector), 'SCC']
+
+###########################################################
+# > unique(SCC[SCC$SCC %in% motorVehicle, 4])
+# [1] Mobile - On-Road Gasoline Light Duty Vehicles
+# [2] Mobile - On-Road Gasoline Heavy Duty Vehicles
+# [3] Mobile - On-Road Diesel Light Duty Vehicles  
+# [4] Mobile - On-Road Diesel Heavy Duty Vehicles  
+# 59 Levels: Agriculture - Crops & Livestock Dust ...
+###########################################################
+
+# Subset for only Vehicle related sources
+NEI_pm25_baltimore_vehicle <- NEI_pm25_baltimore[NEI_pm25_baltimore$SCC %in% motorVehicle,]
+
+q5_data <- ddply(NEI_pm25_baltimore_vehicle, c("year"), summarise,
+                 TotalPerYear    = sum(as.numeric(Emissions)))
+
+plot(q5_data,col="orange",
+	xlab="Year",
+	ylab="PM25",
+	cex=1.2,
+	pch=16,
+	main="Baltimore PM2.5 Emission in years 1999,2002,2005,2008 from motor vehicles sources")
+
+
+
+
+
+# > QUESTION 6 "Total emissions from PM2.5 in Baltimore from 1999 to 2008 from motor Vehicles" < #
+# Get only Baltimore observations
+NEI_pm25_baltimore <- NEI_pm25[NEI_pm25$fips == "24510",]
+NEI_pm25_losangeles <- NEI_pm25[NEI_pm25$fips == "06037",]
+
+# Getting the source IDs from the SDD data frame, only for vehicle combustion-related sources
+motorVehicle <- SCC[grep("Vehicle", SCC$EI.Sector), 'SCC']
+
+###########################################################
+# > unique(SCC[SCC$SCC %in% motorVehicle, 4])
+# [1] Mobile - On-Road Gasoline Light Duty Vehicles
+# [2] Mobile - On-Road Gasoline Heavy Duty Vehicles
+# [3] Mobile - On-Road Diesel Light Duty Vehicles  
+# [4] Mobile - On-Road Diesel Heavy Duty Vehicles  
+# 59 Levels: Agriculture - Crops & Livestock Dust ...
+###########################################################
+
+# Subset for only Vehicle related sources
+NEI_pm25_baltimore_vehicle <- NEI_pm25_baltimore[NEI_pm25_baltimore$SCC %in% motorVehicle,]
+NEI_pm25_losangeles_vehicle <- NEI_pm25_losangeles[NEI_pm25_losangeles$SCC %in% motorVehicle,]
+
+
+q6_data_1 <- ddply(NEI_pm25_baltimore_vehicle, c("year"), summarise,
+                 TotalPerYear    = sum(as.numeric(Emissions)))
+
+q6_data_2 <- ddply(NEI_pm25_losangeles_vehicle, c("year"), summarise,
+                 TotalPerYear    = sum(as.numeric(Emissions)))
+
+
+
+
+
+
+
+
+
+
+
+
+
 
